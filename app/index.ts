e24c4d60499abdd29730f400960ec11e7cf71fb6
kirill <kirkras7356@gmail.com>
2020-02-21 23:16:05 +0200

    init project

---
+++ app/index.ts
@@ -0,0 +1,10 @@
+import express, { Application } from 'express';
+
+const app: Application = express();
+
+const a = 22;
+const bsss = 244;
+
+app.listen(8080, () =>{ 
+    console.log('Running at localhost:8080') 
+});
